b0adee139cc447e593bcb7913f3f976df50e517a
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2026-01-16 20:47:54 +0500

    correct mistake.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Bergschenhoek-Dental/',
    server: {
     host: true, // or '0.0.0.0'
     port: 5173
