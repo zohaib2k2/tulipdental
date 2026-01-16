a5aa5e1b3b5a04df9fff6a1ebc813b267bffcd4c
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2026-01-16 21:01:58 +0500

    fix router.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './context/LanguageContext.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/Bergschenhoek-Dental">
     <LanguageProvider>
       <App />
     </LanguageProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
