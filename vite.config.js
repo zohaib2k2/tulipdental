e23d02f68a1419569a3fb60b7b63791ffe56087b
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2026-02-24 03:11:14 +0500

    added hostname.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tulipdental/',
   plugins: [react()],
    server: {
     host: true, // or '0.0.0.0'
